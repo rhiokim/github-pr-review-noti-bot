4f818f4a5b1e98f279caef8e8b45688789f36d9b
Rhio Kim <rhio.kim@gmail.com>
2017-09-19 21:02:02 +0900

    feat(slack): reduce pull_request slack notification

--- handlers/pull_request.js
+++ handlers/pull_request.js
@@ -1,10 +1,29 @@
 const slack = require("../libs/slack");
 const { getSlackUserNameByGithubId } = require("../libs/helper");
 
-const reduceReviewRequestedWebhook = (reviewers = []) => {
-  return reviewer => {
-    return reviewers.filter(item => item !== reviewer);
-  };
+const _cache = {};
+const reduceReviewRequestedNotification = (prNum, reviewer, next) => {
+  let pr;
+  if (!_cache.hasOwnProperty(prNum)) {
+    _cache[prNum] = {
+      tid: null,
+      reviewers: []
+    };
+  }
+
+  pr = _cache[prNum];
+
+  if (pr.tid) {
+    clearTimeout(pr.tid);
+    pr.tid = undefined;
+  }
+
+  pr.reviewers.push(reviewer);
+  pr.tid = setTimeout(() => {
+    next(pr.reviewers);
+    pr = _cache[prNum] = null;
+    delete _cache[prNum];
+  }, 2000);
 };
 
 module.exports = event => {
@@ -22,14 +41,18 @@ module.exports = event => {
       const pr_link = `<${pull_request.html_url}|${pull_request.title} - #${number} :rocket:>`;
       const pr_from = `from @${getSlackUserNameByGithubId(pull_request.user.login)}`;
 
-      const pr_message = `Hey ${review_to}\nYou got a PR review request ${pr_from} - ${pr_link}\n${pr_body}`;
+      // const pr_message = `Hey ${review_to}\nYou got a PR review request ${pr_from} - ${pr_link}\n${pr_body}`;
 
-      slack.send(pr_message, function(err, res) {
-        if (err) {
-          console.log("Error:", err);
-        } else {
-          console.log("Message sent: ", res);
-        }
+      reduceReviewRequestedNotification(number, requested_reviewer.login, reviewers => {
+        const review_to = reviewers.map(reviewer => `@${getSlackUserNameByGithubId(reviewer)}`).join(", ");
+        const pr_message = `Hey ${review_to}\nYou got a PR review request ${pr_from} - ${pr_link}\n${pr_body}`;
+        slack.send(pr_message, function(err, res) {
+          if (err) {
+            console.log("Error:", err);
+          } else {
+            console.log("Message sent: ", res);
+          }
+        });
       });
       break;
   }
